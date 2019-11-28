ac310095e2a89eff618a7824eb902bd0297428eb
David <blittynet@gmail.com>
2019-11-28 21:04:40 +0100

    update

--- src/app/pipe/search.pipe.ts
+++ src/app/pipe/search.pipe.ts
@@ -7,8 +7,16 @@ export class SearchPipe implements PipeTransform {
   
   transform(list: any[], inputSelection: string, inputType: string): any[] {
     if (inputSelection === undefined || inputType === undefined) return list;
-    console.log(`InputSelection: ${inputSelection}\ninputType: ${inputType}`);
-    return list.filter(book => book.inputType.toUpperCase().includes(inputSelection.toUpperCase()));
+    console.log(`InputSelection: ${inputSelection.toLocaleLowerCase()}\ninputType: ${inputType}`);
+    console.log(`includes: ${inputSelection} ${'Buenos dias LA CASA'.includes(inputSelection.toLocaleUpperCase())}`);
+
+    if (inputType === 'title') return list.filter(book => book.title.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    if (inputType === 'author') return list.filter(book => book.author.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    if (inputType === 'description') return list.filter(book => book.description.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    if (inputType === 'ISBN') return list.filter(book => book.ISBN.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    if (inputType === 'price') return list.filter(book => book.price.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    if (inputType === 'publisher') return list.filter(book => book.publisher.toLocaleUpperCase().includes(inputSelection.toLocaleUpperCase()));
+    //return list.filter(book => book.inputType.includes(inputSelection)); // no funciona
   }
 
 }
